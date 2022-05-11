8b28463bc4a45bc27df03302a85f73b62c28cdc0
Alex <98153476+bgx149@users.noreply.github.com>
2022-05-11 05:48:24 +0300

    2 versio

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-function checkDate(dateAvailable){
-    var date = (new Date());
-    var date1 = (new Date());
-    date.setDate(dateAvailable.substr(0, 2));
-    date.setMonth(dateAvailable.substr(3, 2) -1);
+
+function checkDate(dateAvailable, luukkuNro, kuva){
+    var date2 = (new Date());
+    var text1 = parseFloat(date2.toLocaleDateString());
+    var nyt1 = dateAvailable;
     
-    if ( date <= date1 ){
-    window.alert("ok");
-    } else {
-    window.alert("EI VIELÄ");
-    }
-    }
+      
+  
+  if ( text1 >= nyt1 ){ //tänne pitäs saaha css tyylien poisto ja linkki tapahtumaan 
+      document.getElementById(luukkuNro).style.transform = "rotateY(.5turn)";
+    //  document.getElementById(luukkuNro).style.webkit.transform = "rotateY(0deg)";
+      //document.getElementById(luukkuNro).style.transform.style = "preserve-3d";
+     // document.getElementById(luukkuNro).style.webkit.transform.style = "preserve-3d";
+     $front.toggleClass('.container:hover .front')
+      
+      
+  
+  } else {
+      window.alert("EI VIELÄ");
+  }
+  }
+
+  
+function rotate(dateAvailable, luukkuNro, kuva){
+    var date2 = (new Date());
+    var text1 = parseFloat(date2.toLocaleDateString());
+    var nyt1 = dateAvailable;
+    
+      
+  
+  if ( text1 >= nyt1 ){ //tänne pitäs saaha css tyylien poisto ja linkki tapahtumaan 
+     document.getElementById(luukkuNro).style.transform = "rotateY(180deg)";
+    document.getElementById(luukkuNro).style.webkit.transform = "rotateY(0deg)";
+      document.getElementById(luukkuNro).style.transform.style = "preserve-3d";
+    document.getElementById(luukkuNro).style.webkit.transform.style = "preserve-3d";
+      
+  
+
+     
+      
+ 
+  } else {
+      window.alert("EI VIELÄ");
+  }
+  }
